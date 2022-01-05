64d7f458f554ebf5bf3c9203ce0dd21ddeb169b6
fireduck64 <fireduck@gmail.com>
2022-01-05 14:06:20 -0800

    Update community

--- src/pages/Community.tsx
+++ src/pages/Community.tsx
@@ -3,8 +3,14 @@ function Community() {
       <div className="community main-page">
         (links to discord, reddit and slack)
         <p>
-        Page URLS to maintain:
-        </p>
+					Most of the magic happens in discord.
+					<li>Slack - <a href='https://join.slack.com/t/snowblossom/shared_invite/enQtMzY1OTEzODYzNjk5LWI1NThhMTgzNjA3NjkzMmJkOGMwMzFmYzI5M2I4N2I3MWE3ZmVhNDk1NjM5ZmI0NTUyZWI5MjVmY2NlNGRiNGM'>snowblossom.slack.com</a> <a href='https://archive.snowblossom.org/slack/index.html'>archive</a></li>
+					<li>Discord - <a href='https://discord.gg/nCDMn22DkM'>Snowblossom Discord</a></li>
+					<li>Reddit - <a href='https://reddit.com/r/snowblossom'>/r/snowblossom</a></li>
+					<li>IRC Freenode #snowblossom</li>
+					<li>Youtube - <a href='https://www.youtube.com/channel/UC2ArAOKHi6my2mmOAeIAn6g'>Video channel</a></li>
+					<li>Videos also at: <a href='http://snowblossom-video.snowblossom.io/'>snowblossom-video</a> <a href='https://chan-relay.snowblossom.org/channel/snowblossom-video/'>snowblossom-video</a></li>
+				</p>
         <a>
         https://snowblossom.org/snowfields/index.html
         </a>
